b39e5beec9f478879f46b23c2fe746296e72bdfc
Егор Гаркавый < >
2015-11-15 14:00:52 +0300

    MatrixClass2

--- ClassMatrix/version2/Class1.cs
+++ ClassMatrix/version2/Class1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//пр 
+//пры
 namespace version2
 {
     class sizeExeption : ApplicationException
