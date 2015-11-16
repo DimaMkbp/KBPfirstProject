b692ac6bc9e1f46b95b3750050d28ea960828a68
mmomaster3 <mkbel@mail.ru>
2015-11-16 14:16:50 +0300

    вааыва

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -26,7 +26,7 @@ namespace Matrix
             catch (DefEx ex) { Console.WriteLine(ex); }
             
             Console.ReadLine();
-            //How you do
+            //How you 
         }
     }
 }
