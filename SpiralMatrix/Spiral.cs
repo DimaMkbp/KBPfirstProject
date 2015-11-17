e5a5e14d55ca402460023b2c1888a1f516b052b5
mmomaster3 <mkbel@mail.ru>
2015-11-17 09:48:03 +0300

    Fix bug with from right to left and from bottom to top

--- SpiralMatrix/Spiral.cs
+++ SpiralMatrix/Spiral.cs
@@ -22,44 +22,44 @@ namespace SpiralMatrix
             int k = 0;
             int g = 0;
 
-            while (k <= mass.GetLength(0) * mass.GetLength(1))
+            while (k < mass.GetLength(0) * mass.GetLength(1))
             {
+                Console.WriteLine($"цикл while прошел {g} и k = {k}");
                 Console.ReadLine();
                 for (int j = 0 + g; j < mass.GetLength(1) - g; j++) //слева на право
                 {
                     int i = 0 + g;
                     fill++;
                     mass[i, j] = fill;
-                    Print();
+                    
                     k++;
                 }
-
+                Print();
                 for (int i = 1 + g; i < mass.GetLength(0) - g - 1; i++) //сверху вниз
                 {
                     int j = mass.GetLength(1) - g - 1;
                     fill++;
                     mass[i, j] = fill;
-                    Print();
+                    
                     k++;
                 }
-
-                for (int j = mass.GetLength(1) - 1; j > 0 + g; j--) //справа на лево
+                Print();
+                for (int j = mass.GetLength(1) - 1 - g; j > 0 + g; j--) //справа на лево
                 {
                     int i = mass.GetLength(0) - g - 1;
                     fill++;
                     mass[i, j] = fill;
-                    Print();
                     k++;
                 }
-
-                for (int i = mass.GetLength(0) - 1; i > 0 + g; i--) //сниху вверх
+                Print();
+                for (int i = mass.GetLength(0) - 1 - g; i > 0 + g; i--) //снизу вверх
                 {
                     int j = 0 + g;
                     fill++;
                     mass[i, j] = fill;
-                    Print();
                     k++;
                 }
+                Print();
                 g++;
             }
 
