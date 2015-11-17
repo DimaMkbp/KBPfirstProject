a11ccf5b3d2892f82bf41d3516cedcefc2ec90ff
mmomaster3 <mkbel@mail.ru>
2015-11-17 10:22:14 +0300

    Fix while

--- SpiralMatrix/Spiral.cs
+++ SpiralMatrix/Spiral.cs
@@ -26,24 +26,29 @@ namespace SpiralMatrix
             {
                 Console.WriteLine($"цикл while прошел {g} и k = {k}");
                 Console.ReadLine();
+
                 for (int j = 0 + g; j < mass.GetLength(1) - g; j++) //слева на право
                 {
                     int i = 0 + g;
                     fill++;
                     mass[i, j] = fill;
-                    
+
                     k++;
                 }
                 Print();
+
+                if (fill == mass.GetLength(0) * mass.GetLength(1)) { break; }
+
                 for (int i = 1 + g; i < mass.GetLength(0) - g - 1; i++) //сверху вниз
                 {
                     int j = mass.GetLength(1) - g - 1;
                     fill++;
                     mass[i, j] = fill;
-                    
+
                     k++;
                 }
                 Print();
+
                 for (int j = mass.GetLength(1) - 1 - g; j > 0 + g; j--) //справа на лево
                 {
                     int i = mass.GetLength(0) - g - 1;
@@ -52,6 +57,7 @@ namespace SpiralMatrix
                     k++;
                 }
                 Print();
+
                 for (int i = mass.GetLength(0) - 1 - g; i > 0 + g; i--) //снизу вверх
                 {
                     int j = 0 + g;
